4da65621740803ff437705947b6263ac390382b7
Ilya Lugovcov <ilyalugovcov@gmail.com>
2024-05-15 19:03:45 +0300

    Добавьте файлы проекта.

---
+++ AdmiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdmiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AdmiApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdmiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
